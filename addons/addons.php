a0de4a355acc89e662fcf91ac46c46bcaddff52a
Md Rakibul Islam <irakibul189@gmail.com>
2022-10-07 22:25:05 +0600

    counter and work approach

--- addons/addons.php
+++ addons/addons.php
@@ -241,6 +241,7 @@ final class Bizino_Extension {
 		require_once( BIZINO_ADDONS . '/widgets/bizino-package.php' );
 		require_once( BIZINO_ADDONS . '/widgets/bizino-working-process.php' );
 		require_once( BIZINO_ADDONS . '/widgets/bizino-counter.php' );
+		require_once( BIZINO_ADDONS . '/widgets/bizino-work-approach.php' );
 		require_once( BIZINO_ADDONS . '/widgets/bizino-instagram-gallery.php' );
 		require_once( BIZINO_ADDONS . '/widgets/social-media.php' );
 		require_once( BIZINO_ADDONS . '/widgets/bizino-right-choice.php' );
